e1d9040490ec7bbf3f419e98785c58201e1eea20
a.urbanite <a.urbanite@gmail.com>
2022-05-26 13:43:34 +0200

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore' 
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB41E8FzWEwhyRvvOlHWCaxakKMdnDqOrs",
+  authDomain: "react-blog-30aa0.firebaseapp.com",
+  projectId: "react-blog-30aa0",
+  storageBucket: "react-blog-30aa0.appspot.com",
+  messagingSenderId: "907503171090",
+  appId: "1:907503171090:web:055698127696f38e0a8acb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
