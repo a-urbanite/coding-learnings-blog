46aad78347c0c6fef3738f8daf33d08ec3cd4b87
a.urbanite <a.urbanite@gmail.com>
2022-06-16 14:43:07 +0200

    finished landing page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,36 +20,89 @@ const Home = () => {
           </div>
         </div>
         <div className='CV'>
-          <p>
-                      EDUCATION & TRAINING
-
-              Full Stack JavaScript - School of Applied Technology 
-              Intensive three month training program for full stack web development with a focus on TDD, mob programming, and applied learning.
-
-              Prehistoric Archaeology B.A. + M.A. - Freie Universitat Berlin
-              October 2009 - November 2018 Erasmus in Rome in 2013
-
-              Applied Computer Science B.Sc. - HTW Berlin
-              April 2015 - November 2015 - unfinished
-
-              Cultural Heritage Management Assistent
-              October 2008 - September 2009
-
-
-              WORK EXPERIENCE
-              Webmaster - Deutsches Archaologisches Institut
+          <h2>Links</h2>
+          <ul>
+            <li><a href='https://www.linkedin.com/in/alexander-staedtler' target="_blank" rel="noreferrer">LinkedIn</a></li>
+            <li><a href='https://github.com/a-urbanite' target="_blank" rel="noreferrer">Github</a></li>
+            <li><a href='https://www.staedtler-mapdesign.de' target="_blank" rel="noreferrer">old Portfolio</a></li>
+          </ul>
+          <h2>EDUCATION & TRAINING</h2>
+          <ul>
+            <li>Full Stack JavaScript - School of Applied Technology 
+              Intensive three month training program for full stack web development with a focus on TDD, mob programming, and applied learning.</li>
+            <li>Prehistoric Archaeology B.A. + M.A. - Freie Universitat Berlin
+              October 2009 - November 2018, Erasmus in Rome in 2013</li>
+            <li>Applied Computer Science B.Sc. - HTW Berlin
+              April 2015 - November 2015 - unfinished</li>
+            <li>Cultural Heritage Management Assistent
+              October 2008 - September 2009</li>
+          </ul>
+          <h2>WORK EXPERIENCE</h2>
+          <ul>
+            <li>Webmaster - Deutsches Archaologisches Institut
               April 2020 - December 2021 - Berlin, Germany
-              Technical Management of PR Infrastructure including Wordpress and Liferay CMS, Atlassian stack
-
-              Heritage Consultant - Artefact Pty Ltd.
-              February 2019 - June 2019 - Sydney, NSW Australia Supervising	construction	works	from	an archaeological perspective
-
-              Research Assistant - Deutsches Archaologisches Institut, Freie Universitat Berlin
+              Technical Management of PR Infrastructure including Wordpress and Liferay CMS, Atlassian stack</li>
+            <li>Heritage Consultant - Artefact Pty Ltd.
+              February 2019 - June 2019 - Sydney, NSW Australia Supervising	construction	works	from	an archaeological perspective</li>
+            <li>Research Assistant - Deutsches Archaologisches Institut, Freie Universitat Berlin
               March 2012 - September 2018
               A sequence of employments with archaeological institutes and projects. Activities spanned from office work in Berlin to Field Work in Saudi Arabia, mostly revolving around Geodata and Research Data Management.
-
-          </p>
-
+            </li>
+          </ul>
+          <h2>TECHNICAL SKILLS</h2>
+          <div className='skillListWrapper'>
+            <div className='columnA'>
+                <h3>Languages</h3>
+              <ul>
+                <li>German - native</li>
+                <li>English - fluent</li>
+                <li>Italian - intermediate</li>
+              </ul>
+              <h3>General</h3>
+              <ul>
+                <li>JavaScript</li>
+                <li>Typescript</li>
+                <li>PHP (basic)</li>
+                <li>R (basic)</li>
+              </ul>
+              <h3>Backend</h3>
+              <ul>
+                <li>NodeJS</li>
+                <li>ExpressJS</li>
+                <li>MongoDB/PostgreSQL/GraphQL</li>
+                <li>REST API</li>
+              </ul>
+            </div>
+            <div className='columnB'>
+              <h3>Frontend</h3>
+              <ul>
+                <li>React</li>
+                <li>Redux</li>
+                <li>Webpack</li>
+                <li>HTML/CSS</li>
+              </ul>
+              <h3>Tools and technologies</h3>
+              <ul>
+                <li>Visual Studio Code</li>
+                <li>Docker</li>
+                <li>Git/GitHub/GitHub Actions</li>
+                <li>Cloud Platforms (MongoDB Atlas, ElephantSQL, Heroku, AWS)</li>
+                <li>TDD/BDD with Mocha/Jest</li>
+                <li>Agile work methods CI/CD</li>
+              </ul>
+              <h3>GeoData</h3>
+              <ul>
+                <li>QGIS/ArcGIS</li>
+                <li>Meshlab</li>
+                <li>Metashape</li>
+                <li>WebMapping (Leaflet/Openlayers/ArcGIS Server)</li>
+              </ul>
+            </div>
+          </div>
+          <div className='downloadButtonWrapper'>
+            <a className='downloadButton' href='/w11-extra-CV_alexander_staedtler.pdf' download>CV as PDF</a>
+            <a className='downloadButton' href='/w11-extra-CV_alexander_staedtler.docx' download>CV as DOC</a>
+          </div>
         </div>
     </div>
   )
