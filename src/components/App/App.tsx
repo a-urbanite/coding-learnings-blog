0ea19023d354205ff5ea08dada4ff395a6fd3ba1
a.urbanite <a.urbanite@gmail.com>
2022-10-16 20:27:46 +0200

    removed all non-Blog content

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,17 +5,17 @@ import CreatePost from '../../pages/CreatePost/CreatePost';
 import EditPost from '../../pages/EditPost/EditPost';
 import Login from '../../pages/SignIn/SignIn';
 import Navbar from '../Navbar/Navbar';
-import Home from '../../pages/Home/Home';
-import Archaeology from '../../pages/Archaeology/Archaeology';
+// import Home from '../../pages/Home/Home';
+// import Archaeology from '../../pages/Archaeology/Archaeology';
 import {signOut} from 'firebase/auth'
 import { auth } from '../../firebase-config';
-import PetMeUp from '../../pages/PetMeUp/PetMeUp';
-import DAIWordpressPlugin from '../../pages/DAIWordpressPlugin/DAIWordpressPlugin';
+// import PetMeUp from '../../pages/PetMeUp/PetMeUp';
+// import DAIWordpressPlugin from '../../pages/DAIWordpressPlugin/DAIWordpressPlugin';
 import Pagewrapper from '../PageWrapper/Pagewrapper';
-import MovieFinder from '../../pages/MovieFinder/MovieFinder';
-import Akzisemauer from '../../pages/Akzisemauer/Akzisemauer';
-import TestSite from '../../pages/TestSite/TestSite';
-import LCPA from '../../pages/LCPA/LCPA';
+// import MovieFinder from '../../pages/MovieFinder/MovieFinder';
+// import Akzisemauer from '../../pages/Akzisemauer/Akzisemauer';
+// import TestSite from '../../pages/TestSite/TestSite';
+// import LCPA from '../../pages/LCPA/LCPA';
 import SignUp from '../../pages/SignUp/SignUp';
 import Settings from '../../pages/Settings/Settings';
 import ArticlePage from '../../pages/ArticlePage/ArticlePage'
@@ -38,22 +38,12 @@ function App() {
     <Router>
       <Navbar isAuth={isAuth} setIsAuth={setIsAuth} signUserOut={signUserOut}></Navbar>
       <Routes>
-        <Route path='/' element={<Pagewrapper contents={<Home/>}/>}/>
-        <Route path="/blog" element={<Blog isAuth={isAuth} setPostToEdit={setPostToEdit}/>}/>
+        <Route path="/" element={<Blog isAuth={isAuth} setPostToEdit={setPostToEdit}/>}/>
           <Route path='/blog/:id' element={<Pagewrapper contents={<ArticlePage />}/>}/>
         <Route path="/createpost" element={<CreatePost isAuth={isAuth}/>} />
         <Route path="/editpost" element={<EditPost isAuth={isAuth} postToEdit={postToEdit}/>}/>
         <Route path="/login" element={<Pagewrapper contents={<Login setIsAuth={setIsAuth}/>}/>} />
         <Route path="/signup" element={<Pagewrapper contents={<SignUp/>}/>} />
-        <Route path="/archaeology" element={<Pagewrapper contents={<Archaeology/>}/>} />
-        <Route path="coding">
-          <Route path="pet-me-up" element={<Pagewrapper contents={<PetMeUp/>}/>} />
-          <Route path="moviefinder" element={<Pagewrapper contents={<MovieFinder/>}/>} />
-          <Route path="akzisemauer" element={<Pagewrapper contents={<Akzisemauer/>}/>} />
-          <Route path="dai-wordpress-plugin" element={<Pagewrapper contents={<DAIWordpressPlugin/>}/>} />
-          <Route path="least-cost-path-analysis" element={<Pagewrapper contents={<LCPA/>}/>} />
-        </Route>
-        <Route path="/test-site" element={<Pagewrapper contents={<TestSite/>}/>} />
         <Route path="/settings" element={<Pagewrapper contents={<Settings isAuth={isAuth}/>}/>} />
       </Routes>
     </Router>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,3 @@
-import {Menu, MenuItem, MenuButton, SubMenu} from '@szhsin/react-menu';
-import '@szhsin/react-menu/dist/index.css';
-import '@szhsin/react-menu/dist/transitions/slide.css';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 import { AiFillSetting } from "react-icons/ai";
@@ -10,22 +7,7 @@ const Navbar = ({isAuth, setIsAuth, signUserOut}: any) => {
 
     return (
         <div className='mainNav'>
-            <Link className='mainNav__link' to="/"> Vita </Link>
-            <Menu menuButton={<MenuButton className='mainNav__link' > Coding </MenuButton>} transition>
-                <MenuItem><Link className='mainNav__subLink' to="/coding/pet-me-up"> [TS] Pet me Up! </Link></MenuItem>
-                <MenuItem><Link className='mainNav__subLink' to="/coding/moviefinder"> [TS] Moviefinder </Link></MenuItem>
-                <MenuItem><Link className='mainNav__subLink' to="/coding/akzisemauer"> [webdev/geodata] Akzisemauer </Link></MenuItem>
-                <MenuItem><Link className='mainNav__subLink' to="/coding/dai-wordpress-plugin"> [PHP] Wordpress plugin </Link></MenuItem>
-                <MenuItem><Link className='mainNav__subLink' to="/coding/least-cost-path-analysis"> [R] Least Cost Path analysis </Link></MenuItem>
-                {/* <SubMenu label="Styles">
-                    <MenuItem>about.css</MenuItem>
-                    <MenuItem>home.css</MenuItem>
-                    <MenuItem>index.css</MenuItem>
-                </SubMenu> */}
-            </Menu>
-            <Link className='mainNav__link' to="/archaeology"> Archaeology </Link>
-            { isAuth && <Link className='mainNav__link' to="/test-site"> Test site </Link>}
-            <Link className='mainNav__link' to="/blog"> Blog </Link>
+            <Link className='mainNav__link' to="/"> Blog </Link>
             { !isAuth && <Link className='mainNav__link' to="/login"> Login </Link>}
             { isAuth && <Link className='mainNav__link' to="/createpost"> Create Post </Link>}
             { isAuth && <button className='mainNav__link' onClick={signUserOut}>Log Out</button>}
