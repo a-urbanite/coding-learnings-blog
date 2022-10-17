aa98ddb7240d2464bbef38123857cb11827fc966
a.urbanite <a.urbanite@gmail.com>
2022-10-17 11:17:50 +0200

    changed all keys to env vars

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,10 +8,10 @@ console.log(process.env)
 const firebaseConfig = {
   apiKey:  process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: "react-blog-30aa0",
-  storageBucket: "react-blog-30aa0.appspot.com",
-  messagingSenderId: "907503171090",
-  appId: "1:907503171090:web:055698127696f38e0a8acb"
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
